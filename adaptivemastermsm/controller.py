ff145d337691d3c35de7de81842f70e0492004a2
imitxelena003 <ifrentzua@gmail.com>
2020-08-30 17:31:57 +0200

    controller is able to do all steps, a notebook is included for illustration

--- adaptivemastermsm/controller.py
+++ adaptivemastermsm/controller.py
@@ -14,6 +14,8 @@ import matplotlib.pyplot as plt
 import multiprocessing as mp
 # AdaptiveMasterMSM
 from adaptivemastermsm import controller_lib
+from adaptivemastermsm.system import system
+from adaptivemastermsm.launcher import launcher
 
 class Controller(object):
     """
@@ -61,6 +63,46 @@ class Controller(object):
             raise Exception("It must be 'Production' or 'Equilibration'")
 
         return
+
+    def sys_equilibration(self, gro, forcefield=None, water=None,\
+        topology=None, constraints=None):
+
+        pdb = system.System(gro, forcefield=forcefield, water=water,\
+                 topology=topology, constraints=constraints)
+        # Generate topology file
+        if topology is None:
+            topology='topol.top'
+            if ".gro" in gro:
+                pdb.gen_top(gro=gro, top=topology)
+            else:
+                pdb.gen_top(top=topology)
+        # Build box
+        pdb.build_box(gro=gro, d=self.run['box_size'])
+        # Solvate the system
+        pdb.solvate(top=topology)
+        # Add ions
+        inp = 'conf_solv.gro'
+        if self.run['Cl']>0 or self.run['Na']>0:
+            pdb.ionize(anion=self.run['Cl'], cation=self.run['Na'],\
+                        top=topology)
+            inp = 'conf_solv_ions.gro'
+        # Equilibrate the system, and obtain stable NVT-NPT ensemble
+        pdb.minimize(inp=inp, top=topology)
+        pdb.nvt(mdp='nvt.mdp', top=topology)
+        pdb.npt(mdp='npt.mdp', top=topology)
+
+
+    def sys_production(self, gro, forcefield, water,\
+                        gro_files=None, top_files=None):
+
+        pdb = launcher.Launcher(self.md_step, gro, forcefield, water)
+        # Generate input files for MDRUN
+        if gro_files is None: gro_files='npt.gro'
+        if top_files is None: top_files='topol.top'
+        gmxinp = pdb.inputs(gro_files, top_all=top_files,\
+                    chk='npt.cpt', mdp_params=self.run)
+        # Run simulation using Gromacs
+        pdb.runner(gmxinp)
 
     def adaptive_sampling(self):
         
