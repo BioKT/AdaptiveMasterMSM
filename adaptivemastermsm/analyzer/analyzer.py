9a30f31ec7adf48e87940af1dbd0b8f327b5dd01
imitxelena003 <ifrentzua@gmail.com>
2020-10-05 15:51:56 +0200

    integrate two OPTIONS in gen_inputs

--- adaptivemastermsm/analyzer/analyzer.py
+++ adaptivemastermsm/analyzer/analyzer.py
@@ -6,7 +6,7 @@ This file is part of the AdaptiveMasterMSM package.
 import h5py
 import os, sys
 from os import path
-import itertools
+import random
 # Maths
 import numpy as np
 # Plotting
@@ -15,7 +15,7 @@ import seaborn as sns
 sns.set(style="ticks", color_codes=True, font_scale=1.5)
 sns.set_style({"xtick.direction": "in", "ytick.direction": "in"})
 # Clustering
-from sklearn.preprocessing import StandardScaler
+#from sklearn.preprocessing import StandardScaler
 import hdbscan
 import mdtraj as md
 # AdaptiveMasterMSM
@@ -45,7 +45,8 @@ class Analyzer(object):
         self.data = []
         print(trajfiles.split())
         for f in trajfiles.split():
-            if not path.isfile(f): raise ValueError("Trajectory file(s) not valid")
+            if not path.isfile(f):
+                raise ValueError("Trajectory file(s) not valid")
             if '.xtc' in f:
                 self.data.append(f)
             elif '.h5' in f:
@@ -59,7 +60,7 @@ class Analyzer(object):
 
         # Note: gen_input is only called from outside (controller)
 
-    def build_msm(self, n_runs, lagt, dt=1, mcs=185, ms=145, sym=True,\
+    def build_msm(self, n_runs, lagt, mcs=185, ms=145, dt=1, sym=True,\
                     n_clusters=0, rate_mat=True, gro=None):
         """
         Build the MSM for the next round.
@@ -101,7 +102,6 @@ class Analyzer(object):
                             mcs=self.min_cluster_size, ms=self.min_samples)
             trs.append(tr)
             self.labels_all.append(labels)
-        
         self.trajs = trs
 
         #2- do MSM:
@@ -138,15 +138,15 @@ class Analyzer(object):
         
         tr.find_keys()
         tr.keys.sort()
-        """
-        fig, ax = plt.subplots(figsize=(10,3))
-        ax.plot(tr.mdt.time, [x for x in tr.distraj], lw=2)
-        ax.set_xlim(0,5000)
-        ax.set_ylim(0.8,2.2)
-        ax.set_xlabel('Time (ps)', fontsize=20)
-        ax.set_ylabel('state', fontsize=20)
-        plt.savefig('traj.png')
-        """
+#        """
+#        fig, ax = plt.subplots(figsize=(10,3))
+#        ax.plot(tr.mdt.time, [x for x in tr.distraj], lw=2)
+#        ax.set_xlim(0,5000)
+#        ax.set_ylim(0.8,2.2)
+#        ax.set_xlabel('Time (ps)', fontsize=20)
+#        ax.set_ylabel('state', fontsize=20)
+#        plt.savefig('traj.png')
+#        """
         return tr.distraj, tr
 
     def gen_clusters_mueller(self, i):
@@ -190,7 +190,7 @@ class Analyzer(object):
 
         return labels
 
-    def gen_msm(self, tr_instance=False):
+    def gen_msm(self, tr_instance=False): #, dt=None, lagt=None, sym=False, rate=False):
         """
         Do MSM and build macrostates
 
@@ -200,72 +200,63 @@ class Analyzer(object):
             True if an instance of trajectory exists
 
         """
-
+        lagt, dt, sym, rate = self.lt, self.dt, self.sym, self.rate
         # Create an instance of traj for each trajectory and merge in a single list
         if tr_instance:
             distrajs = self.trajs
         else:
             labels = self.labels_all
-            dt = self.dt
             distrajs = []
             for label in labels:
                 distraj = traj.TimeSeries(distraj=list(label), dt=dt)
                 distraj.find_keys()
                 distraj.keys.sort()
                 distrajs.append(distraj)
-
-        lagt = self.lt
-        sym = self.sym
         # Invoke SuperMSM to collect keys from all trajectories
         smsm = msm.SuperMSM(distrajs, sym=sym)
         micro_msm = msm.MSM(data=distrajs, keys=smsm.keys, lagt=lagt, sym=sym)
         micro_msm.do_count()
-        if self.rate:
+        if rate:
             #smsm = msm.SuperMSM(distrajs, sym=True) #keys=distraj.keys
             smsm.do_lbrate()
             micro_msm.do_rate(method='MLPB', evecs=False, init=smsm.lbrate)
         else:
             micro_msm.do_trans(evecs=False)
-        
-        fig, ax = plt.subplots()
-        #ax.errorbar(range(1,2),np.log(micro_msm.tauK[0:2]), fmt='o-')
-        ax.errorbar(range(1,2),micro_msm.tauT[0:10], fmt='o-')
-        ax.set_xlabel('Eigenvalue index')
-        ax.set_ylabel(r'$\tau_i$ (ns)')
-        plt.savefig('eigs_T.png')
-        
+#        fig, ax = plt.subplots()
+#        #ax.errorbar(range(1,2),np.log(micro_msm.tauK[0:2]), fmt='o-')
+#        ax.errorbar(range(1,2),micro_msm.tauT[0:10], fmt='o-')
+#        ax.set_xlabel('Eigenvalue index')
+#        ax.set_ylabel(r'$\tau_i$ (ns)')
+#        plt.savefig('eigs_T.png')       
         self.MSM = micro_msm
 
-    def gen_mmsm(self):
-        """
-        Build macrostates
-
-        """
-
-        n_clusters = self.n_clusters
-        MSM = self.MSM
-        mmsm = fewsm.FEWSM(MSM, N=n_clusters)
-        self.mMSM = mmsm
-
-        import matplotlib.cm as cm
-        fig, ax = plt.subplots(figsize=(5,5))
-        mat = np.zeros((20,20), float)
-        for i in MSM.keep_keys:
-            j = MSM.keep_keys.index(i)
-            if j in mmsm.macros[0]:
-                mat[i%20, int(i/20)] = 1
-            elif j in mmsm.macros[1]:
-                mat[i%20, int(i/20)] = 2
-            else:
-                mat[i%20, int(i/20)] = 3
-            mat#print i, i[0]%20, int(i[0]/20), -i[1]
-        my_cmap = cm.get_cmap('viridis')
-        my_cmap.set_under('w')
-        ax.imshow(mat.transpose(), interpolation="none", origin='lower', \
-             cmap=my_cmap, vmin = 0.5)
-        plt.savefig('fewms.png')
+#    def gen_macro_msm(self, n_clusters=1):
+#        """
+#        Build macrostates
+#
+#        """
+#        MSM = self.MSM
+#        macro_msm = fewsm.FEWSM(MSM, N=n_clusters)
+#        self.mMSM = macro_msm
+#
+#        fig, ax = plt.subplots(figsize=(5,5))
+#        mat = np.zeros((20,20), float)
+#        for i in MSM.keep_keys:
+#            j = MSM.keep_keys.index(i)
+#            if j in mmsm.macros[0]:
+#                mat[i%20, int(i/20)] = 1
+#            elif j in mmsm.macros[1]:
+#                mat[i%20, int(i/20)] = 2
+#            else:
+#                mat[i%20, int(i/20)] = 3
+#            mat#print i, i[0]%20, int(i[0]/20), -i[1]
+#        my_cmap = cm.get_cmap('viridis')
+#        my_cmap.set_under('w')
+#        ax.imshow(mat.transpose(), interpolation="none", origin='lower', \
+#             cmap=my_cmap, vmin = 0.5)
+#        plt.savefig('fewms.png')
         
-    def resampler(self, scoring='populations'):
+    def resampler(self, scoring='populations', sym=False):
         """
         
         Parameters
@@ -280,38 +271,53 @@ class Analyzer(object):
         elif scoring == "populations":
             states = self.populs()
         elif scoring == "non_detailed_balance":
-            if self.sym: raise Exception("Cannot impose symmetry with chosen scoring criteria.")
+            if sym: 
+                raise Exception("Cannot impose symmetry with chosen scoring criteria.")
             states = self.non_detbal()
 
-        self.gen_input(states)
+        inputs = self.gen_input(states)
 
-    def non_detbal(self):
-        """
-        Resampling criteria proportional to lack of detailed
-        balance of each MSM state
-
-        """
-
-        nondb = abs( self.MSM.count - np.transpose(self.MSM.count) )
-        return np.sum(nondb, axis=1)        
+        return inputs
 
     def populs(self):
         """
-        Resampling criteria inversely proportional to
-        MSM's populations
+        Resampling probability inversely proportional to
+        state populations
 
         """
-
-        return 1./self.MSM.peqK if self.rate else 1./self.MSM.peqT
+        p = 1./self.MSM.peqT
+        return p/np.sum(p)
 
     def counts(self):
         """
-        Resampling criteria inversely proportional to
-        how many times those states have been visited
+        Resampling probability inversely proportional to
+        number of visits
 
         """
+        p = 1./np.sum(self.MSM.count, axis=1)
+        return p/np.sum(p)
 
-        return 1./np.sum(self.MSM.count, axis=1)
+    def non_detbal(self):
+        """
+        Resampling probability proportional to lack of detailed
+        balance for each MSM state
+
+        """
+        total = self.MSM.count + np.transpose(self.MSM.count)
+        nondb = abs(self.MSM.count - np.transpose(self.MSM.count))/total
+        nondb = np.sum(np.nan_to_num(nondb, 0), axis=1)
+        return nondb/np.sum(nondb)
+
+    def flux_inbalance(self):
+        """
+        Resampling probality proportional to flux imbalance
+
+        """
+        flux = [(np.sum(self.MSM.count[x, :]) - np.sum(self.MSM.count[:,x]))/\
+                (np.sum(self.MSM.count[:,x]) + np.sum(self.MSM.count[x, :])) \
+                for x in range(len(self.MSM.keep_states))]
+        flux += np.min(flux)
+        return flux/np.sum(flux)
 
     def gen_input(self, states):
         """
@@ -323,28 +329,35 @@ class Analyzer(object):
         states : np array
             Scoring of each macrostate
         n_msm_runs : int array
-            Number of runs for each macrostate
+            List of labels from which generate new inputs
+        n_msm_runs_aux : int array
+            Number of new runs for each label
 
         """
         # Determine distribution of new runs according to 'states'
-        n_runs = self.n_runs
-        n_msm_runs = []
-        for x_i in states:
-            if np.log(x_i) > 0.5:
-                n_msm_runs.append(np.log(x_i))
-            else:
-                n_msm_runs.append(0.0)
-        fac = n_runs / np.sum(n_msm_runs)
-        n_msm_runs = [fac*x_i for x_i in n_msm_runs]
-        n_msm_runs = [round(x_i) for x_i in n_msm_runs]
+        n_runs = self.n_runs #n_runs = 10
+        n_msm_runs = np.random.choice(range(len(self.MSM.keep_states)), n_runs, p=states)
+        print (n_msm_runs)
         
-        print(len(n_msm_runs), np.sum(n_msm_runs))
-        print(n_msm_runs, n_runs)
+#        # OPTION 1: Use weights to randomly choose a frame and create a corresponding .gro file
+#        n_msm_runs_aux = []
+#        for i in self.MSM.keep_states:
+#            n_msm_runs_aux.append(n_msm_runs.count(i))
+#        inputs = analyzer_lib.gen_input_weights(n_msm_runs_aux, self.labels_all, self.trajs)
+
+        # OPTION 2: Use a dict containing all trajs, labels and frames to pick randomly a frame
+        state_kv = {}
+        inputs = []
+        for n in n_msm_runs:
+            if n not in state_kv.keys():
+                print ("Building entry for microstate %g"%n)
+                analyzer_lib.gen_dict_state(n, self.trajs, state_kv)
+            traj, frame = random.choice(state_kv[n])
+            analyzer_lib.map_inputs(traj, n, frame, inputs)
+            #print (n, traj, frame)
+
+        return inputs
+
+
         
-        # Use weights to randomly choose a frame and create a corresponding .gro file
-        analyzer_lib.gen_input_weights(n_msm_runs, self.labels_all, self.trajs)
-
-        # Use a dict containing all trajs, labels and frames to pick randomly a frame
-        # to do ...
-
 
--- adaptivemastermsm/analyzer/analyzer_lib.py
+++ adaptivemastermsm/analyzer/analyzer_lib.py
@@ -3,8 +3,10 @@ This file is part of the AdaptiveMasterMSM package.
 
 """
 
+import sys, os
 import numpy as np
 import random
+from adaptivemastermsm.launcher import launcher_lib
 #import mdtraj as md
 
 def gen_input_weights(n_msm_runs, labels_all, trajs):
@@ -31,6 +33,7 @@ def gen_input_weights(n_msm_runs, labels_all, trajs):
             w[j][i] = len(index)
         if np.sum(w[j]) > 0.0: w[j] /= np.sum(w[j])
 
+    inputs = []
     for i in range(len(n_msm_runs)):
         while True:
             which_tr_index = random.choices(range(len(labels_all)), weights=w[:,i])
@@ -39,28 +42,53 @@ def gen_input_weights(n_msm_runs, labels_all, trajs):
             index = list_duplicates_of(list(which_tr), i)
             if len(index) is 0: continue # esto es redundante si weights funciona
             j = random.choice(index)
-            map_inputs(trajs, which_tr_index[0], j)
+            map_inputs(trajs[which_tr_index[0]].mdt, i, j, inputs)
             counter[which_tr[j]] += 1
             if n_msm_runs[i] <= counter[which_tr[j]]: break
         
     print(np.sum(counter), counter)
+    print(inputs)
+    return inputs
 
-def map_inputs(trajs, which_tr_index, state):
+def map_inputs(traj, label, frame, inputs):
     """
         
     Parameters
     ----------
-    which_tr_index : int
-        Which trajectory identified by index
-    state : int
-        Index corresponding to the initial state for the new input
+    label : int
+        Label identifying the initial state of the new input
+    frame : int
+        Frame corresponding to the initial state for the new input
 
     """
 
-    #print(type(self.trajs[which_tr_index].mdt), type(self.trajs[which_tr_index].mdt[state]))
-    traj = trajs[which_tr_index].mdt
-    fn = '%s_%s.gro'%(which_tr_index,state)
-    traj[state].save_gro(fn)
+    fn = '%s_%s.gro'%(label,frame)
+    inputs.append(fn)
+    #traj[frame].save_gro(fn) #traj[frame].center_coordinates()
+    tr = 'data/tr_%s.xtc'%(frame)
+    traj[frame].save_xtc(tr)
+    tpr = 'data/alaTB.tpr'
+    print('ionix',traj[frame].time,traj.time[frame])
+    cmd = "gmx trjconv -s %s -f %s -dump %s"%(tpr, tr, traj.time[frame])
+    #cmd = "gmx trjconv -f %s -dump %f"%(tr, traj[frame].time)
+    os.system(cmd)
+
+    """# frame file
+    frame = 'data/frame_%s_%s.ndx'%(label,frame)
+    launcher_lib.checkfile(frame)
+    inp = open(frame, "w")
+    txt = """
+    #`[frames]
+    #%d
+    """ % (frame)
+    inp.write(txt)
+    inp.close()
+    # trajectory file
+    tr = 'data/tr_%s.xtc'%(frame)
+    traj[frame].save_xtc(tr)
+    # create gro file corresponding to snapshot frame from tr trajectory
+    cmd = "gmx trjconv -f %s -o %s -fr %s"%(tr, fn, frame)
+    os.system(cmd)"""
 
 
 def list_duplicates_of(seq,item):
@@ -75,3 +103,35 @@ def list_duplicates_of(seq,item):
             locs.append(loc)
             start_at = loc
     return locs
+
+#def gen_input_dict(n_msm_runs, self.labels_all, self.trajs):
+#    state_kv = {}
+#    for n in n_msm_runs:
+#        if n not in state_kv.keys():
+#            print ("Building entry for microstate %g"%n)
+#            gen_dict_state(n, an)
+#        traj, frame = random.choice(state_kv[n])
+#        #print (n, traj, frame)
+#    return state_kv
+
+def gen_dict_state(s, trajs, state_kv):
+    """
+    Generates dictionary entry for state s
+    
+    Parameters
+    ----------
+    s : str, int
+        The key for the state
+    trajs : instance
+        Instance of TimeSeries containing all trajectories
+
+    """
+    #global state_kv
+    state_kv[s] = []
+    for t in trajs:
+        try:
+            ivals = np.where(t.distraj == s)[0]
+            for i in ivals:
+                state_kv[s].append([t.mdt, i])
+        except KeyError:
+            pass
